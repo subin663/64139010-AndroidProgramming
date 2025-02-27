6d44e2b536e18fd17d49e1d27bd5a4f98acd0c54
Vinh <lvinh5559@gmail.com>
2025-02-27 22:13:01 +0700

    update

--- app/src/main/java/com/example/oclinear/MainActivity.java
+++ app/src/main/java/com/example/oclinear/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.oclinear;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,27 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void XuLyCong(View view)
+    {
+        EditText edtA = findViewById(R.id.edt1);
+        EditText edtB = findViewById(R.id.edt2);
+        EditText edtKQ = findViewById(R.id.edtKetQua);
+        //Lấy dữ liệu ở điều khiển A
+        String strA = edtA.getText().toString(); //strA=2;
+        //Lấy dữ liệu ở điều khiển B
+        String strB = edtB.getText().toString(); //strB=4;
+
+        //Chuyển dữ liệu sang dạng số
+        int so_A= Integer.parseInt(strA);   //2
+        int so_B= Integer.parseInt(strB);   //4
+        //Tính toán
+        int ans=so_A+so_B;
+
+        //Chuyển ans sang dạng string để xuất dữ liệu
+        String strKQ=String.valueOf(ans); //strKQ=6
+        //Xuất dữ liệu ra màn hình
+        edtKQ.setText(strKQ);
+
     }
 }
