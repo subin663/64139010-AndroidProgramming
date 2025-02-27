508b1a71068388a7255129db2a2d75ae21cf4f63
Vinh <lvinh5559@gmail.com>
2025-02-27 17:52:16 +0700

    UpdateEx3
    Hoàn thành chương trình cộng

--- Ex3_SimpleSumApp/app/src/main/java/com/example/ex3_simplesumapp/MainActivity.java
+++ Ex3_SimpleSumApp/app/src/main/java/com/example/ex3_simplesumapp/MainActivity.java
@@ -12,14 +12,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    Button nutTong;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        Timdieukhien();
-        
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -27,22 +26,29 @@ public class MainActivity extends AppCompatActivity {
         });
     }
     //note
-    public void Timdieukhien()
+    public void Xu_Ly_Cong(View view)
     {
         EditText edtextA = findViewById(R.id.edtA);
         EditText edtextB = findViewById(R.id.edtB);
         EditText editKQ = findViewById(R.id.edtKQ);
-        String strA = edtextB.getText().toString(); //strA=2;
+        //Lấy dữ liệu ở điều khiển A
+        String strA = edtextA.getText().toString(); //strA=2;
+        //Lấy dữ liệu ở điều khiển B
         String strB = edtextB.getText().toString(); //strB=4;
+
+        //Chuyển dữ liệu sang dạng số
+        int so_A= Integer.parseInt(strA);   //2
+        int so_B= Integer.parseInt(strB);   //4
+        //Tính toán
+        int ans=so_A+so_B;
+
+        //Chuyển ans sang dạng string để xuất dữ liệu
+        String strKQ=String.valueOf(ans); //strKQ=6
+        //Xuất dữ liệu ra màn hình
+        editKQ.setText(strKQ);
+
     }
-    
-    public void Xu_Ly_Cong(View v){
-        
-    }
-    
-    public void Xu_Ly_Tru(View v){
-        
-    }
-    
-    
+
+
+
 }
