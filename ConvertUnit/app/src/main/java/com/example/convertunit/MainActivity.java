53a2845bf55ac19be322ee5183b10980364e051c
Vinh <lvinh5559@gmail.com>
2025-03-02 12:36:07 +0700

    UdPrjConvertUnit

--- ConvertUnit/app/src/main/java/com/example/convertunit/MainActivity.java
+++ ConvertUnit/app/src/main/java/com/example/convertunit/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.convertunit;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +12,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText edt;
+    EditText edtAns;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +24,23 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        TimDieuKhien();
     }
+    public void TimDieuKhien(){
+        edt=findViewById(R.id.edtDola);
+        edtAns=findViewById(R.id.edtAns);
+    }
+    public void btnConvert(View view)
+    {
+        String strA = edt.getText().toString();
+
+        double Dola=Double.parseDouble(strA);
+        //Tính toán
+        double ans=Dola/25;
+
+        String strKQ=String.valueOf(ans); //strKQ=6
+        edtAns.setText(strKQ);
+
+    }
+
 }
