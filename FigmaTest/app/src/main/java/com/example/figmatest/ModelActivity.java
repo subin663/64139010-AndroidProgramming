38895d51629e62d69b823540114e6359110d2e26
Vinh <lvinh5559@gmail.com>
2025-03-20 16:11:30 +0700

    ud

---
+++ FigmaTest/app/src/main/java/com/example/figmatest/ModelActivity.java
@@ -0,0 +1,32 @@
+package com.example.figmatest;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ModelActivity extends AppCompatActivity {
+    Button btnCount;
+    Button btnMatch;
+    Button btnPattern;
+    Button btnSettings;
+    void TimDieuKhien(){
+        btnCount=findViewById(R.id.btnCou);
+        
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_model);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
