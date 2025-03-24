9148a37cc4888c09df49a70a09a9394c21254955
Vinh <lvinh5559@gmail.com>
2025-03-24 23:09:27 +0700

    ud

--- Ex13_RecyclerView/app/src/main/java/com/example/ex13_recyclerview/MainActivity.java
+++ Ex13_RecyclerView/app/src/main/java/com/example/ex13_recyclerview/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -20,5 +21,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        //3
+        recyclerViewDatas=getDataForRecyclerView();
+        //4 Tìm điều khiển
+        rycyclerViewLanscape=findViewById(R.id.recyclerLand);
+        //5
+        RecyclerView.LayoutManager layoutLinear=new LinearLayoutManager(this);
+        rycyclerViewLanscape.setLayoutManager(layoutLinear);
+        //6
+        landScapeAdapter=new LandScapeAdapter(this,recyclerViewDatas);
+        //7
+        rycyclerViewLanscape.setAdapter(landScapeAdapter);
+    }
+
+    ArrayList<LandScape> getDataForRecyclerView(){
+        ArrayList<LandScape> dsDuLieu=new ArrayList<LandScape>();
+        LandScape landScape1=new LandScape("img","StreetImage");
+        dsDuLieu.add(landScape1);
+        dsDuLieu.add(new LandScape("img_1","MoutainImage"));
+        dsDuLieu.add(new LandScape("img_2","LakeImage"));
+        dsDuLieu.add(new LandScape("img_3","SpringImage"));
+        return dsDuLieu;
+
     }
 }
